d35b488764681ca7df4990717dbd25f65ddd82ae
Brandon Cornel <bcc4679@rit.edu>
2017-12-28 16:28:24 -0500

    Init

---
+++ App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+import Movies from './src/Movies';
+
+
+const App = StackNavigator({
+    Movies: { screen: Movies}
+})
+
+export default App;
---
+++ src/Movies.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import {
+  ScrollView,
+  View
+}from 'react-native';
+
+import {movies} from './data';
+import MoviePoster from './MoviePoster';
+
+export default class Movies extends Component{
+  render(){
+    return(
+      <View>
+        <ScrollView>
+        {movies.map((movie, index) => <MoviePoster
+  movie={movie}
+  onOpen={this.openMovie}
+  key={index}
+    />)}
+        </ScrollView>
+      </View>
+    )
+  }
+}
